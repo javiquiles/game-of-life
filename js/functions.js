f26116c8fab4cb70cedda2d129eac8383a61ca05
javiquiles <javiquiles@hotmail.com>
2017-06-09 00:41:40 -0300

    Seteamos estados de las celdas y contamos las vivas

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,18 @@
-var listado, estado, activos, cuenta = 0
+var listado, estados, activos, cuenta = 0
 var dim = {
   ancho: 10,
   alto: 10
 };
 
 function createTable() {
-    estado = [];
+    estados = []; //Definimos el array estado.
 
     for (var x = 0; x < dim.alto; x++) {
         document.write("<tr>");
-        estado[x] = [];
+        estados[x] = []; //Definimos la matriz estado.
 
         for (var y = 0; y < dim.ancho; y++) {
-            estado[x][y] = false;
+            estados[x][y] = false;
             document.write("<td class='' onclick='cambiarCelda(this)'></td>");
         }
         document.write("</tr>");
@@ -20,8 +20,8 @@ function createTable() {
 }
 
 function cambiarCelda(celda) {
-    if(celda.className == '') {
-        celda.className = 'vivo'
+    if (celda.className == '') {
+        celda.className = 'viva'
     }
     else {
         celda.className = ''
@@ -29,7 +29,7 @@ function cambiarCelda(celda) {
 }
 
 function sgtePaso() {
-    listado = document.getElementsByTagName('td');
+    listado = document.getElementsByTagName('td'); //Array de 100 celdas.
     console.log(listado[3]);
 
     var bool;
@@ -38,10 +38,15 @@ function sgtePaso() {
 
     for (var i = 0; i < dim.alto; i++) {
         for (var j = 0; j < dim.ancho; j++) {
-            bool = (listado[dim.ancho * i + j].className == 'vivo');
-            estado[i][j] = bool;
+            /* Comprobamos los estados de todas las celdas */
+            bool = (listado[dim.ancho * i + j].className == 'viva');
+            estados[i][j] = bool;
 
-            if(bool) activos++;
+            if (bool) activos++;
         }
+    }
+
+    if (activos == 0) {
+        console.log('No hay mas celdas vivas');
     }
 }
